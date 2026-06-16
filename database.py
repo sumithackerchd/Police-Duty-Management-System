7636f2967865f7f8aff52c45b8faea62b9b58ab4
Sumit Goswami <sumitgiri.chandausi@gmail.com>
2026-06-16 11:33:28 +0530

    Added Search Officer feature

--- database.py
+++ database.py
@@ -41,3 +41,42 @@ def view_duties():
     conn.close()
 
     return duties
+def update_duty(duty_id, officer_name, duty_location, duty_date):
+    conn = sqlite3.connect("duty.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE duties
+    SET officer_name=?, duty_location=?, duty_date=?
+    WHERE id=?
+    """, (officer_name, duty_location, duty_date, duty_id))
+
+    conn.commit()
+    conn.close()
+
+
+def delete_duty(duty_id):
+    conn = sqlite3.connect("duty.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM duties WHERE id=?",
+        (duty_id,)
+    )
+
+    conn.commit()
+    conn.close()
+    def search_officer(name):
+    conn = sqlite3.connect("duty.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM duties WHERE officer_name LIKE ?",
+        ('%' + name + '%',)
+    )
+
+    results = cursor.fetchall()
+
+    conn.close()
+
+    return results
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from database import create_database, add_duty, view_duties
+from database import (
+    create_database,
+    add_duty,
+    view_duties,
+    update_duty,
+    delete_duty,
+    search_officer
+)
 
 create_database()
 
@@ -6,10 +13,14 @@ print("===== Police Duty Management System =====")
 
 print("1. Add Duty")
 print("2. View Duties")
+print("3. Update Duty")
+print("4. Delete Duty")
+print("5. Search Officer")
 
 choice = input("Enter Choice: ")
 
 if choice == "1":
+
     name = input("Enter Officer Name: ")
     location = input("Enter Duty Location: ")
     date = input("Enter Duty Date (DD-MM-YYYY): ")
@@ -27,5 +38,37 @@ elif choice == "2":
     for duty in duties:
         print(duty)
 
+elif choice == "3":
+
+    duty_id = int(input("Enter Duty ID: "))
+    name = input("New Officer Name: ")
+    location = input("New Duty Location: ")
+    date = input("New Duty Date: ")
+
+    update_duty(duty_id, name, location, date)
+
+    print("Duty updated successfully!")
+
+elif choice == "4":
+
+    duty_id = int(input("Enter Duty ID to delete: "))
+
+    delete_duty(duty_id)
+
+    print("Duty deleted successfully!")
+
+
+elif choice == "5":
+
+    name = input("Enter Officer Name: ")
+
+    results = search_officer(name)
+
+    print("\n===== Search Results =====")
+
+    for officer in results:
+        print(officer)
+
 else:
+
     print("Invalid Choice")
